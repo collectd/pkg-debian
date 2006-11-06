e859716e1f3cb3210171fd340c08637ed50ea08d
Sebastian Harl <sh@tokkee.org>
2006-11-06 16:00:02 +0000

    Added Required-{Start,Stop} keywords to init script.

--- debian/collectd.init.d
+++ debian/collectd.init.d
@@ -9,7 +9,9 @@
 ### BEGIN INIT INFO
 # Provides:          collectd
 # Required-Start:    $local_fs $remote_fs
+# Required-Stop:     $local_fs $remote_fs
 # Should-Start:      $network $named $syslog $time
+# Should-Stop:       $network $named $syslog
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
 # Short-Description: start the statistics collection daemon
